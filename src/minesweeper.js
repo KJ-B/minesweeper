d5140d7ec8afd20a1dfb4ae005736e43c07665de
Your Name <you@example.com>
2018-02-05 14:02:31 -0500

    minesweeper

---
+++ src/minesweeper.js
@@ -0,0 +1,11 @@
+const blankLine = '  |   |  no';
+console.log("This is what an empty board looks like:");
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+const guessLine = '1 |   |  ';
+const bombLine = '  | B |  ';
+console.log("This what a board with a guess and a bomb on it would look like:");
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
