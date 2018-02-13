3a4f88028217bf4d2141594fe7d25bf85c3e1161
Your Name <you@example.com>
2018-02-13 14:49:40 -0500

    incrementing operator

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -11,3 +11,25 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
 };
 
 console.log(generatePlayerBoard(5, 4));
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    const row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+  const numberOfBombsPlaced = 0;
+    while (numberOfBombsPlaced < numberOfBombs) {
+      let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+      let randomIndColumnIndex = Math.floor(Math.random() * numberOfColumns);
+      board[[randomRowIndex],[randomIndColumnIndex]] = 'B';
+      for (numberOfBombsPlacedIndex = 0; numberOfBombsPlacedIndex < numberOfBombs; numberOfBombsPlacedIndex++) {
+
+      };
+    }
+
+  return board;
+};
