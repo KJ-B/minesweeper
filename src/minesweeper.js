0db320e08f694e5f7d5ed4aebd7467fdeeb89b0a
Your Name <you@example.com>
2018-02-13 11:41:49 -0500

    loops pon loops

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,21 +1,9 @@
-/*const printBoard = (board) => {
-  console.log('Current Board:');
-  console.log(board[0].join(' | '));
-};*/
-const unoBoard = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-];
 
-const printBoard = board => {
-  console.log('Current Board: ');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  const board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    for (let columnIndex = 0; columnIndex < numberOfColumns)
+  }
 };
 
-printBoard(unoBoard);
-unoBoard[0][1] = 1;
-unoBoard[1][0] = 'B';
-printBoard(unoBoard);
+console.log(board);
