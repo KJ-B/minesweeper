a441dab3acdf8304b4256ebea339995d83c1307b
Your Name <you@example.com>
2018-02-13 12:29:31 -0500

    row push board push return board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,12 +1,13 @@
-
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   const board = [];
   for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
     const row = [];
-    row.push(' ');
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(' ');
+    }
     board.push(row);
   }
   return board;
 };
 
-console.log(generatePlayerBoard(4, 3));
+console.log(generatePlayerBoard(5, 4));
